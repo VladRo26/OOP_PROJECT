858f55fe20cb5d76808e20a5d5a46ee4c4b4ba83
VladRo26 <Libelula262326>
2022-12-07 02:09:39 +0200

    Am pus explicit in constuctori

--- Exceptii.h
+++ Exceptii.h
@@ -12,17 +12,17 @@ class eroare_aplicatie : public std::runtime_error {
 
 class input_invalid : public eroare_aplicatie {
 public:
-    input_invalid(const std::string &string_) : eroare_aplicatie("Input invalid: " + string_) {}
+    explicit input_invalid(const std::string &string_) : eroare_aplicatie("Input invalid: " + string_) {}
 };
 
 class null_ptr : public eroare_aplicatie {
 public:
-    null_ptr(const std::string &string_) : eroare_aplicatie("Pointerul este null: " + string_) {}
+    explicit null_ptr(const std::string &string_) : eroare_aplicatie("Pointerul este null: " + string_) {}
 };
 
 class eroare_constructor : public input_invalid {
 public:
-    eroare_constructor(const std::string &string_) : input_invalid("Eroare Constuctor: " + string_) {}
+    explicit eroare_constructor(const std::string &string_) : input_invalid("Eroare Constuctor: " + string_) {}
 };
 
 
